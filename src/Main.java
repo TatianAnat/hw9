08d8bde8939ff4ff880e52c1ba05108fcbf68103
TatianAnat <TatianAnat@yandex.ru>
2025-04-18 20:06:40 +0400

    homework 9 task 1

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //task 1
         System.out.println("Task 1");
-        int contributionPercent = 29000;
-        int percent = 0;
-        for(int i = 1; i <= 12; i++) {
-            percent =  contributionPercent +  contributionPercent / 100;
-            contributionPercent = percent;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + contributionPercent + " рублей");
+        int savings = 0;
+        int month = 0;
+       while (savings < 2_459_000) {
+           savings =  savings +  15_000;
+           month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
     }
 }
 }
