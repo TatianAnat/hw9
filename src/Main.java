70c6f13a9097328d7b483ddf007e84728ccd0f75
TatianAnat <TatianAnat@yandex.ru>
2025-04-20 19:56:49 +0400

    homework 9 task 6

--- src/Main.java
+++ src/Main.java
@@ -57,11 +57,14 @@ public class Main {
         System.out.println("Task 6");
         int summaN = 15_000;
         int percentN = 7;
-        float sixMonths= 0;
-        while (sixMonths < 9) {
-            sixMonths = sixMonths + 0.5f;
-            summaN = (summaN / 100) * (percentN / 2) + summaN;
-            System.out.println("Месяц " + sixMonths + " сумма накопления равна " + summaN);
+        int sixMonths= 0;
+        while (sixMonths < 108) {
+            sixMonths++;
+            summaN = (summaN / 100) * percentN + summaN;
+            if (sixMonths % 6 == 0) {
+                System.out.println("Месяц " + sixMonths + " сумма накопления равна " + summaN);
+            }
+
             }
         //task 7
 //        System.out.println("Task 7");
